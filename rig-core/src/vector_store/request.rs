414a66234a20d21c38e7a90c376f7224e12aabd8
Fay Carsons <faycarsons23@gmail.com>
2025-10-13 13:34:41 -0400

    Create vector search filter eDSL

--- rig-core/src/vector_store/request.rs
+++ rig-core/src/vector_store/request.rs
@@ -4,7 +4,7 @@ use super::VectorStoreError;
 
 /// A vector search request - used in the [`super::VectorStoreIndex`] trait.
 #[derive(Clone, Serialize, Deserialize, Debug)]
-pub struct VectorSearchRequest {
+pub struct VectorSearchRequest<Filter = ConcreteFilter> {
     /// The query to be embedded and used in similarity search.
     query: String,
     /// The maximum number of samples that may be returned. If adding a similarity search threshold, you may receive less than the inputted number if there aren't enough results that satisfy the threshold.
@@ -13,12 +13,17 @@ pub struct VectorSearchRequest {
     threshold: Option<f64>,
     /// Any additional parameters that are required by the vector store.
     additional_params: Option<serde_json::Value>,
+    /// An expression used to filter samples
+    filter: Option<Filter>,
 }
 
-impl VectorSearchRequest {
+impl<Filter> VectorSearchRequest<Filter>
+where
+    Filter: SearchFilter,
+{
     /// Creates a [`VectorSearchRequestBuilder`] which you can use to instantiate this struct.
-    pub fn builder() -> VectorSearchRequestBuilder {
-        VectorSearchRequestBuilder::default()
+    pub fn builder() -> VectorSearchRequestBuilder<Filter> {
+        VectorSearchRequestBuilder::<Filter>::default()
     }
 
     /// The query to be embedded and used in similarity search.
@@ -36,16 +41,97 @@ impl VectorSearchRequest {
     }
 }
 
+pub trait SearchFilter {
+    type Key;
+    type Value;
+
+    fn eq(key: Self::Key, value: Self::Value) -> Self;
+    fn gt(key: Self::Key, value: Self::Value) -> Self;
+    fn lt(key: Self::Key, value: Self::Value) -> Self;
+    fn and(self, rhs: Self) -> Self;
+    fn or(self, rhs: Self) -> Self;
+}
+
+/// A canonical, serializable retpresentation of filter expressions.
+/// This serves as an intermediary form whenever you need to inspect,
+/// store, or translate between specific vector store backends
+#[derive(Debug, Clone, Serialize, Deserialize)]
+#[serde(rename_all = "lowercase")]
+pub enum ConcreteFilter {
+    Eq(String, serde_json::Value),
+    Gt(String, serde_json::Value),
+    Lt(String, serde_json::Value),
+    And(Box<Self>, Box<Self>),
+    Or(Box<Self>, Box<Self>),
+}
+
+impl SearchFilter for ConcreteFilter {
+    type Key = String;
+    type Value = serde_json::Value;
+
+    fn eq(key: Self::Key, value: Self::Value) -> Self {
+        Self::Eq(key, value)
+    }
+
+    fn gt(key: Self::Key, value: Self::Value) -> Self {
+        Self::Gt(key, value)
+    }
+
+    fn lt(key: Self::Key, value: Self::Value) -> Self {
+        Self::Lt(key, value)
+    }
+
+    fn and(self, rhs: Self) -> Self {
+        Self::And(self.into(), rhs.into())
+    }
+
+    fn or(self, rhs: Self) -> Self {
+        Self::Or(self.into(), rhs.into())
+    }
+}
+
+impl ConcreteFilter {
+    fn interpret<F>(self) -> F
+    where
+        F: SearchFilter<Value = serde_json::Value>,
+        F::Key: From<String>,
+    {
+        match self {
+            Self::Eq(key, val) => F::eq(key.into(), val),
+            Self::Gt(key, val) => F::gt(key.into(), val),
+            Self::Lt(key, val) => F::lt(key.into(), val),
+            Self::And(lhs, rhs) => F::and(lhs.interpret(), rhs.interpret()),
+            Self::Or(lhs, rhs) => F::or(lhs.interpret(), rhs.interpret()),
+        }
+    }
+}
+
 /// The builder struct to instantiate [`VectorSearchRequest`].
-#[derive(Clone, Serialize, Deserialize, Debug, Default)]
-pub struct VectorSearchRequestBuilder {
+#[derive(Clone, Serialize, Deserialize, Debug)]
+pub struct VectorSearchRequestBuilder<F = ConcreteFilter> {
     query: Option<String>,
     samples: Option<u64>,
     threshold: Option<f64>,
     additional_params: Option<serde_json::Value>,
+    filter: Option<F>,
 }
 
-impl VectorSearchRequestBuilder {
+impl<F> Default for VectorSearchRequestBuilder<F> {
+    fn default() -> Self {
+        Self {
+            query: None,
+            samples: None,
+            threshold: None,
+            additional_params: None,
+            filter: None,
+        }
+    }
+}
+
+impl<F> VectorSearchRequestBuilder<F>
+where
+    F: SearchFilter,
+{
     /// Set the query (that will then be embedded )
     pub fn query<T>(mut self, query: T) -> Self
     where
@@ -71,6 +157,11 @@ impl VectorSearchRequestBuilder {
     ) -> Result<Self, VectorStoreError> {
         self.additional_params = Some(params);
         Ok(self)
+    }
+
+    pub fn filter(mut self, filter: F) -> Self {
+        self.filter = Some(filter);
+        self
     }
 
     pub fn build(self) -> Result<VectorSearchRequest, VectorStoreError> {
@@ -102,6 +193,7 @@ impl VectorSearchRequestBuilder {
             samples,
             threshold: self.threshold,
             additional_params,
+            filter: None,
         })
     }
 }
